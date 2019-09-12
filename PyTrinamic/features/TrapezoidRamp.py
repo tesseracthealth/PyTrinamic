9f744583ed10ba4b18177bd5a9d2f9444df02038
Leonard Kugis <leonard@kug.is>
2019-09-12 16:33:53 +0200

    TrapezoidRamp
    Introduce new way of feature based method calls.
    TrapezoidRamp().setTargetVelocity(50000, motors[0])
    This is interrupt safe, since the axis number gets derived directly via
    motor and not buffered until operation.

--- PyTrinamic/features/TrapezoidRamp.py
+++ PyTrinamic/features/TrapezoidRamp.py
@@ -2,29 +2,30 @@
 # Created on: 21.08.2019
 
 from PyTrinamic.features.Feature import Feature
+from PyTrinamic.Motor.Motor import Motor
 
 class TrapezoidRamp(Feature):
-    def setTargetPosition(self, value, axis):
-        raise NotImplementedError()
-    def getTargetPosition(self, axis):
-        raise NotImplementedError()
-    def setActualPosition(self, value, axis):
-        raise NotImplementedError()
-    def getActualPosition(self, axis):
-        raise NotImplementedError()
-    def setTargetVelocity(self, value, axis):
-        raise NotImplementedError()
-    def getTargetVelocity(self, axis):
-        raise NotImplementedError()
-    def setActualVelocity(self, value, axis):
-        raise NotImplementedError()
-    def getActualVelocity(self, axis):
-        raise NotImplementedError()
-    def setMaximumVelocity(self, value, axis):
-        raise NotImplementedError()
-    def getMaximumVelocity(self, axis):
-        raise NotImplementedError()
-    def setMaximumAcceleration(self, value, axis):
-        raise NotImplementedError()
-    def getMaximumAcceleration(self, axis):
-        raise NotImplementedError()
+    def setTargetPosition(self, value, motor):
+        return motor[TrapezoidRamp].setTargetPosition(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def getTargetPosition(self, motor):
+        return motor[TrapezoidRamp].getTargetPosition(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def setActualPosition(self, value, motor):
+        return motor[TrapezoidRamp].setActualPosition(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def getActualPosition(self, motor):
+        return motor[TrapezoidRamp].getActualPosition(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def setTargetVelocity(self, value, motor):
+        return motor[TrapezoidRamp].setTargetVelocity(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def getTargetVelocity(self, motor):
+        return motor[TrapezoidRamp].getTargetVelocity(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def setActualVelocity(self, value, motor):
+        return motor[TrapezoidRamp].setActualVelocity(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def getActualVelocity(self, motor):
+        return motor[TrapezoidRamp].getActualVelocity(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def setMaximumVelocity(self, value, motor):
+        return motor[TrapezoidRamp].setMaximumVelocity(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def getMaximumVelocity(self, motor):
+        return motor[TrapezoidRamp].getMaximumVelocity(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def setMaximumAcceleration(self, value, motor):
+        return motor[TrapezoidRamp].setMaximumAcceleration(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
+    def getMaximumAcceleration(self, motor):
+        return motor[TrapezoidRamp].getMaximumAcceleration(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
