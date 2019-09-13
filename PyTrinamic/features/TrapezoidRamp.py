83d17c495bbf2c4c4a66954e42986d4e36891ad5
Leonard Kugis <leonard@kug.is>
2019-09-13 14:31:41 +0200

    Features, TrapezoidRamp
    Introduced new feature parameter calling mechanic.
    TrapezoidRamp.setTargetVelocity(50000, motors[0])
    
    This is just a new possibility for convenience, others are the same.

--- PyTrinamic/features/TrapezoidRamp.py
+++ PyTrinamic/features/TrapezoidRamp.py
@@ -5,27 +5,39 @@ from PyTrinamic.features.Feature import Feature
 from PyTrinamic.Motor.Motor import Motor
 
 class TrapezoidRamp(Feature):
-    def setTargetPosition(self, value, motor):
+    @staticmethod
+    def setTargetPosition(value, motor):
         return motor[TrapezoidRamp].setTargetPosition(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def getTargetPosition(self, motor):
+    @staticmethod
+    def getTargetPosition(motor):
         return motor[TrapezoidRamp].getTargetPosition(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def setActualPosition(self, value, motor):
+    @staticmethod
+    def setActualPosition(value, motor):
         return motor[TrapezoidRamp].setActualPosition(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def getActualPosition(self, motor):
+    @staticmethod
+    def getActualPosition(motor):
         return motor[TrapezoidRamp].getActualPosition(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def setTargetVelocity(self, value, motor):
+    @staticmethod
+    def setTargetVelocity(value, motor):
         return motor[TrapezoidRamp].setTargetVelocity(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def getTargetVelocity(self, motor):
+    @staticmethod
+    def getTargetVelocity(motor):
         return motor[TrapezoidRamp].getTargetVelocity(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def setActualVelocity(self, value, motor):
+    @staticmethod
+    def setActualVelocity(value, motor):
         return motor[TrapezoidRamp].setActualVelocity(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def getActualVelocity(self, motor):
+    @staticmethod
+    def getActualVelocity(motor):
         return motor[TrapezoidRamp].getActualVelocity(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def setMaximumVelocity(self, value, motor):
+    @staticmethod
+    def setMaximumVelocity(value, motor):
         return motor[TrapezoidRamp].setMaximumVelocity(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def getMaximumVelocity(self, motor):
+    @staticmethod
+    def getMaximumVelocity(motor):
         return motor[TrapezoidRamp].getMaximumVelocity(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def setMaximumAcceleration(self, value, motor):
+    @staticmethod
+    def setMaximumAcceleration(value, motor):
         return motor[TrapezoidRamp].setMaximumAcceleration(value, motor.getFeatureProviderMeta(TrapezoidRamp)[2])
-    def getMaximumAcceleration(self, motor):
+    @staticmethod
+    def getMaximumAcceleration(motor):
         return motor[TrapezoidRamp].getMaximumAcceleration(motor.getFeatureProviderMeta(TrapezoidRamp)[2])
