190f3b2cfaad92ebcd4c5b22c70fe51f3f3c5f06
Leonard Kugis <leonard@kug.is>
2021-05-26 21:49:54 +0200

    TMCM1240: Inheriting feature hierarchy.

--- PyTrinamic/modules/TMCM1240/TMCM_1240.py
+++ PyTrinamic/modules/TMCM1240/TMCM_1240.py
@@ -5,10 +5,13 @@ Created on 21.09.2020
 '''
 
 from PyTrinamic.modules.tmcl_module import tmcl_module
+from PyTrinamic.features.StallGuard2Module import StallGuard2Module
+from PyTrinamic.features.LinearRampModule import LinearRampModule
+from PyTrinamic.features.MotorControl import MotorControl
 
-class TMCM_1240(tmcl_module):
+class TMCM_1240(tmcl_module, StallGuard2Module, LinearRampModule, MotorControl):
 
-    class APs():
+    class APs:
         TargetPosition                 = 0
         ActualPosition                 = 1
         TargetVelocity                 = 2
@@ -61,7 +64,7 @@ class TMCM_1240(tmcl_module):
         SG2Threshold                   = 174
         GlobalCurrentScaler            = 178
         smartEnergyActualCurrent       = 180
-        smartEnergyStallVelocity       = 181
+        SmartEnergyStallVelocity       = 181
         smartEnergyThresholdSpeed      = 182
         RandomTOffMode                 = 184
         ChopperSynchronization         = 185
@@ -94,10 +97,10 @@ class TMCM_1240(tmcl_module):
         step_direction_mode            = 254
         unit_mode                      = 255
 
-    class ENUMs():
+    class ENUMs:
         FLAG_POSITION_END = 0x00004000
 
-    class GPs():
+    class GPs:
         timer_0                        = 0
         timer_1                        = 1
         timer_2                        = 2
@@ -129,13 +132,13 @@ class TMCM_1240(tmcl_module):
         Intpol                         = 255
 
     def __init__(self, connection, module_id=1):
-        super().__init__(connection, module_id)
+        tmcl_module.__init__(self, connection, module_id)
 
         self.MOTORS = 1
         self.__default_motor = 0
 
     def showChipInfo(self):
-        print("The TMCM-1240 is a single axis controller/driver module. Voltage supply: 24V");
+        print("The TMCM-1240 is a single axis controller/driver module. Voltage supply: 24V")
 
     " motion Control functions "
     def rotate(self, axis, velocity):
@@ -158,7 +161,7 @@ class TMCM_1240(tmcl_module):
 
     " current control functions "
     def setMotorRunCurrent(self, axis, current):
-        self.setMaxCurrent(current)
+        self.setMaxCurrent(axis, current)
 
     def setMotorStandbyCurrent(self, axis, current):
         self.setAxisParameter(self.APs.StandbyCurrent, axis, current)
@@ -169,50 +172,7 @@ class TMCM_1240(tmcl_module):
     def setMaxCurrent(self, axis, current):
         self.setAxisParameter(self.APs.MaxCurrent, axis, current)
 
-    " StallGuard2 functions "
-    def setStallguard2Filter(self, axis, enableFilter):
-        self.setAxisParameter(self.APs.SG2FilterEnable, axis, enableFilter)
-
-    def setStallguard2Threshold(self, axis, threshold):
-        self.setAxisParameter(self.APs.SG2Threshold, axis, threshold)
-
-    def setStopOnStallVelocity(self, axis, velocity):
-        self.setAxisParameter(self.APs.smartEnergyStallVelocity, axis, velocity)
-
     " motion parameter functions "
-    def getTargetPosition(self, axis):
-        return self.axisParameter(self.APs.TargetPosition, axis, signed=True)
-
-    def setTargetPosition(self, axis, position):
-        self.setAxisParameter(self.APs.TargetPosition, axis, position)
-
-    def getActualPosition(self, axis):
-        return self.axisParameter(self.APs.ActualPosition, axis, signed=True)
-
-    def setActualPosition(self, position):
-        return self.setAxisParameter(self.APs.ActualPosition, axis, position)
-
-    def getTargetVelocity(self, axis):
-        return self.axisParameter(self.APs.TargetVelocity, axis, signed=True)
-
-    def setTargetVelocity(self, axis, velocity):
-        self.setAxisParameter(self.APs.TargetVelocity, axis, velocity)
-
-    def getActualVelocity(self, axis):
-        return self.axisParameter(self.APs.ActualVelocity, axis, signed=True)
-
-    def getMaxVelocity(self, axis):
-        return self.axisParameter(self.APs.MaxVelocity, axis)
-
-    def setMaxVelocity(self, axis, velocity):
-        self.setAxisParameter(self.APs.MaxVelocity, axis, velocity)
-
-    def getMaxAcceleration(self, axis):
-        return self.axisParameter(self.APs.MaxAcceleration, axis)
-
-    def setMaxAcceleration(self, axis, acceleration):
-        self.setAxisParameter(self.APs.MaxAcceleration, axis, acceleration)
-
     def setMicrostepResolution(self, axis, microstepResolution):
         self.setAxisParameter(self.APs.MicrostepResolution, axis, microstepResolution)
 
